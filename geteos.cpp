78b6fd7b74e8eeebee494bb68159c720eaec49fa
root <root@localhost.localdomain>
2019-07-19 16:18:49 +0900

    get balance

---
+++ geteos.cpp
@@ -0,0 +1,34 @@
+#include<eosio/eosio.hpp>
+#include<eosio/asset.hpp>
+
+using namespace eosio;
+
+CONTRACT geteos: public contract {
+    public:
+        using contract::contract;
+
+        ACTION getbalance(name user, asset token) {
+            accounts mytoken("eosio.token"_n, user.value);
+            symbol sym = token.symbol;
+            auto itr = mytoken.require_find(sym.code().raw());
+
+            print(itr->balance);
+        }
+    private:
+        struct [[eosio::table]] account {
+                asset    balance;
+
+                uint64_t primary_key()const { return balance.symbol.code().raw(); }
+            };
+
+            struct [[eosio::table]] currency_stats {
+                asset    supply;
+                asset    max_supply;
+                name     issuer;
+
+                uint64_t primary_key()const { return supply.symbol.code().raw(); }
+            };
+
+            typedef eosio::multi_index< "accounts"_n, account > accounts;
+            typedef eosio::multi_index< "stat"_n, currency_stats > stats;
+    };
